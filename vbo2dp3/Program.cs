c48cf40fac0a05d57170c854bef93591f7f6f5af
satorimon <48250071+satorimon@users.noreply.github.com>
2023-06-07 21:31:50 +0900

    Added display of exception details

--- vbo2dp3/Program.cs
+++ vbo2dp3/Program.cs
@@ -9,29 +9,39 @@ namespace vbo2dp3
     {
         static void Main(string[] args)
         {
-            if (args.Length < 1)
+            try
             {
-                Console.WriteLine("引数が有りません。処理を終了します。");
-                return;
-            }
-            foreach (var s in args)
-            {
-                Console.WriteLine(s);
-                var inputPath = s;
-                var filename = Path.GetFileNameWithoutExtension(inputPath);
-                var outputPath = Path.GetDirectoryName(inputPath) + Path.DirectorySeparatorChar
-                    + filename + ".dp3";
-
-                var records = Vbo2GpsRecord.ReadVbo(inputPath);
-                var bytes = dp3Converter.Vbo2dp3(records);
-                using (var stream = File.Open(outputPath, FileMode.Create))
-                using (var writer = new BinaryWriter(stream))
+                if (args.Length < 1)
                 {
-                    writer.Write(bytes.ToArray());
-                    writer.Flush();
-                    writer.Close();
-                    
+                    Console.WriteLine("引数が有りません。処理を終了します。");
+                    return;
                 }
+                foreach (var s in args)
+                {
+                    Console.WriteLine(s);
+                    var inputPath = s;
+                    var filename = Path.GetFileNameWithoutExtension(inputPath);
+                    var outputPath = Path.GetDirectoryName(inputPath) + Path.DirectorySeparatorChar
+                        + filename + ".dp3";
+
+                    var records = Vbo2GpsRecord.ReadVbo(inputPath);
+                    var bytes = dp3Converter.Vbo2dp3(records);
+                    using (var stream = File.Open(outputPath, FileMode.Create))
+                    using (var writer = new BinaryWriter(stream))
+                    {
+                        writer.Write(bytes.ToArray());
+                        writer.Flush();
+                        writer.Close();
+
+                    }
+
+                }
+            }catch(Exception ex)
+            {
+                Console.WriteLine("例外が発生したため処理を中止しました。");
+                Console.WriteLine(ex);
+                Console.WriteLine("処理を中断するには何かキーを押してください。");
+                Console.ReadKey();
 
             }
         }
